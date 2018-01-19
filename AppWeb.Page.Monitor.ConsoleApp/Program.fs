61f4c03ed8b292fc3555a28423a8e076cb7cab49
Mattias Andersson <mattiasandersson88@hotmail.com>
2018-01-19 17:42:32 +0100

    Added the monitor project and an simple example console app project

---
+++ AppWeb.Page.Monitor.ConsoleApp/Program.fs
@@ -0,0 +1,34 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open AppWeb.Page.Monitor
+open DomainTypes
+
+let uri1: PageMonitorUri = {Uri = new Uri("https://www.appweb.se"); MonitorMethod = MonitorMethodUnion.HttpGet}
+let uri2: PageMonitorUri = {Uri = new Uri("https://tinkr.cloud"); MonitorMethod = MonitorMethodUnion.HttpGet}
+let uri3: PageMonitorUri = {Uri = new Uri("https://test.tinkr.cloud"); MonitorMethod = MonitorMethodUnion.HttpGet}
+let uri4: PageMonitorUri = {Uri = new Uri("https://notvalidurl.tinkr.cloud"); MonitorMethod = MonitorMethodUnion.HttpGet}
+
+let uriConfigurations: list<PageMonitorUri> = [uri1; uri2; uri3; uri4]
+
+let printMonitorResult result = printfn "%A" result 
+
+let runMonitor = 
+    let runResult = 
+        Monitor.run 
+            uriConfigurations
+    List.map 
+        printMonitorResult 
+        runResult
+    
+let readLine = 
+    printfn 
+        "%A" 
+        (Console.ReadLine() |> string)
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine 
+        runMonitor
+    readLine
+    0
